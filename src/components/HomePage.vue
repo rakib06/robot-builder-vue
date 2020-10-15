d68ac58ccf1e2bcbf40ac509097495e9cca8f896
Rakibul Hasan <six.rakib@gmail.com>
2020-10-15 12:32:37 +0600

    Initial Commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png"  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 25px;
+}
+</style>
