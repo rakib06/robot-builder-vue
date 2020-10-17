a6301fd375a2af7952630120265cd8b076ad087c
Rakibul Hasan <six.rakib@gmail.com>
2020-10-17 13:02:26 +0600

    routing: Using Named Views and a Sidebar

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,24 @@
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Build'}" exact>
-           
+            <router-link class="nav-link" :to="{name: 'Build'}" exact> 
             Build
             </router-link>
           </li>
         </ul>
       </nav>
     </header>
-    <main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
      
       <router-view/>
     </main>
+    </div>
+   
+    
   </div>
 </template>
 
@@ -37,30 +43,25 @@ export default {
 </script>
 
 <style>
-.nav-link{
-  text-decoration: none;
-  color: inherit;
-}
-.router-link-active{
-  color: rgb(219, 29, 108);
-}
 body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
 }
+</style>
+
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -77,5 +78,24 @@ ul {
   vertical-align: middle;
   height: 30px;
 }
-
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
+}
+.container {
+display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
+}
 </style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,69 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage';
 import RobotBuilder from '../builder/RobotBuilder';
 import PartInfo from '../parts/PartInfo';
+import BrowseParts from '../parts/BrowseRouter';
+import RobotArms from '../parts/RobotArms'
+import RobotBases from '../parts/RobotBases'
+import RobotHeads from '../parts/RobotHeads'
+import RobotTorsos from '../parts/RobotTorsos'
+import SidebarBuild from '../sidebars/SidebarBuild';
+import SidebarStandard from '../sidebars/SidebarStandard';
+
+
 Vue.use(Router);
 
 export default new Router({
     routes:[{
         path:'/',
         name: 'Home',
-        component:HomePage
+        components:{
+            default: HomePage,
+            sidebar: SidebarStandard
+        }
     },
     {
         path:'/build',
         name: 'Build',
-        component: RobotBuilder,
+        components:{
+            default: RobotBuilder,
+            sidebar: SidebarBuild
+        }
+    },
+    // Params must be declared later **** IMPORTANT
+    {
+        path: '/parts/browse',
+        name: 'BrowseParts',
+        component: BrowseParts,
+        children:[
+            {
+                name:  'BrowseHeads',
+                path: 'heads',
+                component: RobotHeads,
+            },
+            {
+                name:  'BrowseArms',
+                path: 'arms',
+                component: RobotArms,
+            },
+            {
+                name:  'BrowseTorsos',
+                path: 'torsos',
+                component: RobotTorsos,
+            },
+            {
+                name:  'BrowseBases',
+                path: 'bases',
+                component: RobotBases,
+            }
+        ]
+
     },
     {
         path:'/parts/:partType/:id',
         name: 'Parts',
         component: PartInfo,
         props: true,
-    }
+    },
+
     ]
 })
